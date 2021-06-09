0280499efbfece860a2404b6b548d9a12012b5d3
gzocchi <z.giorgio.z@gmail.com>
2021-06-09 15:06:17 +0200

    aggiunto header pagina

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="container-fluid">
+    <!-- <img :src="logo" alt="Logo"> -->
+    <img src="../assets/img/spotify.png" alt="Logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+      return {
+        //   logo: "../assets/img/spotify.png",
+      }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/general";
+
+header {
+  height: $header_h;
+  background-color: $bg_light;
+
+  img{
+      height: 100%;
+      padding: 5px;
+  }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class=" container p-5 text-center">
+    <h1>Vue Dischi</h1>
+    <h2>Main</h2>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/general";
+
+main {
+  height: $main_h;
+}
+</style>
