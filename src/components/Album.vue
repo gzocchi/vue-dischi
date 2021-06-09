55b9ef4848bd55f99287bf26dc3caa1c3319f5f4
gzocchi <z.giorgio.z@gmail.com>
2021-06-09 16:11:14 +0200

    aggiunto main e album section

---
+++ src/components/Album.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <img :src="item.poster" alt="item.title" />
+    <h3 class="my-3">{{ item.title }}</h3>
+    <p>
+      {{ item.author }} <br />
+      {{ item.year }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Album",
+  props: ["item"],
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/general";
+
+div {
+  height: 100%;
+  padding: 10px;
+  background-color: $bg_light;
+
+  img {
+    width: 100%;
+  }
+
+  h3 {
+    font-weight: 700;
+    font-size: 1.2em;
+    text-transform: uppercase;
+  }
+  p {
+    color: rgba($text_color, 0.6);
+  }
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,39 @@
 <template>
-  <main class=" container p-5 text-center">
-    <h1>Vue Dischi</h1>
-    <h2>Main</h2>
+  <main class="container p-3 text-center">
+    <section class="gallery">
+      <div class="album" v-for="(album, index) in albums" :key="index">
+        <Album :item="album" />
+      </div>
+    </section>
   </main>
 </template>
 
 <script>
+import Album from "./Album.vue";
+
+import axios from "axios";
+
 export default {
   name: "Main",
+  components: {
+    Album,
+  },
+  data() {
+    return {
+      apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+      albums: [],
+    };
+  },
+  created() {
+    axios
+      .get(this.apiUrl)
+      .then((response) => {
+        this.albums = response.data.response;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
 };
 </script>
 
@@ -16,5 +42,21 @@ export default {
 
 main {
   height: $main_h;
+
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+    height: 100%;
+    width: 70%;
+    margin: 0 auto;
+    padding: 20px 0;
+
+    .album {
+      width: 20%;
+      max-height: calc(100% / 2);
+      flex-shrink: 0;
+      padding: 10px 15px;
+    }
+  }
 }
 </style>
